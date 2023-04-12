ceb1be5663ac9657c72350c8068b286fed290791
G-Lumia <giorgiolumia@gmail.com>
2023-04-12 14:39:00 +0200

    hello world created

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <h1>
+    {{text}}
+  </h1>
+</template>
+<script>
+export default {
+  name : 'AppHeader',
+  data(){
+        return {
+            text: 'La mia prima applicazione con Vite!'
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class>
+        <div class="container">
+            poop
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name : 'AppJumbo'
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
